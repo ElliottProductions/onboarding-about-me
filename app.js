3224042368ecd81e254788011d2cf2f6be387f84
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-01 15:37:17 -0800

    readme edit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 /// grab DOM elements we want to change by id.
 const showButton = document.getElementById('btn');
 const animal = document.getElementById('animalDiv');
-/// console.log(showButton, animal, 'validate to make sure we grabbed them')
+console.log(showButton, animal, 'validate to make sure we grabbed them')
 // set event listeners
 showButton.addEventListener('click', () => {
     console.log(' show button clicked', 'validate button is connected')
-    animal.classList.remove('hidden');
+    animal.classList.toggle('hidden');
 });
